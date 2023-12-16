048c945e5aa6ab1eb9516db3ef7b8d0989dbb9d5
mseo39 <a01063294905@gmail.com>
2023-12-16 15:03:04 +0900

    feat: 바로 앞사람과 같은 사람인지 검사

--- src/main/java/oncall/model/Matching.java
+++ src/main/java/oncall/model/Matching.java
@@ -3,6 +3,7 @@ package oncall.model;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.SimpleTimeZone;
 
 public class Matching {
     private List<String> workerMatching;
@@ -14,5 +15,12 @@ public class Matching {
         this.month = Month.getMonth(month);
         this.workerMatching = new ArrayList<>();
         this.holiday = Holiday.getHoliday(this.month);
+    }
+
+    public boolean checkFrontWorker(String name){
+        if(workerMatching.get(workerMatching.size()-1).equals(name)){
+            return false;
+        }
+        return true;
     }
 }
