a69e065fe02d6a0354c717df5b32ad5e01ec37e8
mseo39 <a01063294905@gmail.com>
2023-12-16 14:47:26 +0900

    feat: 평일인지 아닌지 알려주는 기능

---
+++ src/main/java/oncall/model/Day.java
@@ -0,0 +1,24 @@
+package oncall.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum Day {
+    WEEKDAY("평일", Arrays.asList("월", "화", "수", "목", "금")),
+    WEEKEND("휴일", Arrays.asList("토", "일"));
+
+    private String type;
+    private List<String> days;
+
+    Day(String type, List<String> days) {
+        this.type = type;
+        this.days = days;
+    }
+
+    public static boolean isWeekday(String input){
+        if(WEEKDAY.days.contains(input)){
+            return true;
+        }
+        return false;
+    }
+}
